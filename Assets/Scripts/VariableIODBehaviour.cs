687cf563b35eb8fac7d495960aa3c505d85bb1b1
Ramin Atrian <ratrian@ucsd.edu>
2021-05-16 13:56:46 -0700

    progress for part #6

--- Assets/Scripts/VariableIODBehaviour.cs
+++ Assets/Scripts/VariableIODBehaviour.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class VariableIODBehaviour : MonoBehaviour
 {
+    float iod;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        iod = 0.065f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        //GameObject.Find("OVRCameraRig").GetComponent<P2Utils>().setIODDistance(OVRInput.Get(OVRInput.Axis2D.SecondaryThumbstick).x);
+        if (OVRInput.GetDown(OVRInput.Button.SecondaryThumbstick))
+        {
+            iod = 0.065f;
+        }
+        else
+        {
+            iod += OVRInput.Get(OVRInput.Axis2D.SecondaryThumbstick).x;
+        }
+        GameObject.Find("OVRCameraRig").GetComponent<P2Utils>().setIODDistance(iod);
     }
 }
