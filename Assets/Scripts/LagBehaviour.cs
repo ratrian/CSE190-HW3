ce29103f2a4be769f3d4f3cb96388c14d8bd92d4
Ramin Atrian <ratrian@ucsd.edu>
2021-05-17 00:41:15 -0700

    progress for parts #7 and #8

--- Assets/Scripts/LagBehaviour.cs
+++ Assets/Scripts/LagBehaviour.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class LagBehaviour : MonoBehaviour
 {
+    int numFramesTrackingLag, numFramesRenderingDelay;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        numFramesTrackingLag = 0;
+        numFramesRenderingDelay = 0;
     }
 
     // Update is called once per frame
@@ -16,21 +19,33 @@ public class LagBehaviour : MonoBehaviour
         // Tracking Lag
         if (OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger))
         {
-
+            if (numFramesTrackingLag > 0)
+            {
+                numFramesTrackingLag--;
+            }
+            //
         }
         else if (OVRInput.GetDown(OVRInput.Button.SecondaryIndexTrigger))
         {
-
+            numFramesTrackingLag++;
+            //
         }
+        GameObject.Find("TrackingLag").GetComponent<UnityEngine.UI.Text>().text = "Tracking Lag:\n" + numFramesTrackingLag.ToString() + " frames";
 
         // Rendering Lag
         if (OVRInput.GetDown(OVRInput.Button.PrimaryHandTrigger))
         {
-
+            if (numFramesRenderingDelay > 0)
+            {
+                numFramesRenderingDelay--;
+            }
+            //
         }
         else if (OVRInput.GetDown(OVRInput.Button.SecondaryHandTrigger))
         {
-
+            numFramesRenderingDelay++;
+            //
         }
+        GameObject.Find("RenderingDelay").GetComponent<UnityEngine.UI.Text>().text = "Rendering Delay:\n" + numFramesRenderingDelay.ToString() + " frames";
     }
 }
