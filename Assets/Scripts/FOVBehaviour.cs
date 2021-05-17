9e56ec0893cf6602323dd9616ea3d429b7d9f92f
Ramin Atrian <ratrian@ucsd.edu>
2021-05-16 21:07:04 -0700

    done with part #6

--- Assets/Scripts/FOVBehaviour.cs
+++ Assets/Scripts/FOVBehaviour.cs
@@ -7,14 +7,23 @@ public class FOVBehaviour : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        GameObject.Find("LeftQuad").GetComponent<Renderer>().enabled = false;
-        GameObject.Find("RightQuad").GetComponent<Renderer>().enabled = false;
-        GameObject.Find("TopQuad").GetComponent<Renderer>().enabled = false;
-        GameObject.Find("BottomQuad").GetComponent<Renderer>().enabled = false;
-        GameObject.Find("TopLeftQuad").GetComponent<Renderer>().enabled = false;
-        GameObject.Find("TopRightQuad").GetComponent<Renderer>().enabled = false;
-        GameObject.Find("BottomLeftQuad").GetComponent<Renderer>().enabled = false;
-        GameObject.Find("BottomRightQuad").GetComponent<Renderer>().enabled = false;
+        GameObject.Find("LeftQuad1").GetComponent<Renderer>().enabled = true;
+        GameObject.Find("RightQuad1").GetComponent<Renderer>().enabled = true;
+        GameObject.Find("TopQuad1").GetComponent<Renderer>().enabled = true;
+        GameObject.Find("BottomQuad1").GetComponent<Renderer>().enabled = true;
+        GameObject.Find("TopLeftQuad1").GetComponent<Renderer>().enabled = true;
+        GameObject.Find("TopRightQuad1").GetComponent<Renderer>().enabled = true;
+        GameObject.Find("BottomLeftQuad1").GetComponent<Renderer>().enabled = true;
+        GameObject.Find("BottomRightQuad1").GetComponent<Renderer>().enabled = true;
+
+        GameObject.Find("LeftQuad2").GetComponent<Renderer>().enabled = true;
+        GameObject.Find("RightQuad2").GetComponent<Renderer>().enabled = true;
+        GameObject.Find("TopQuad2").GetComponent<Renderer>().enabled = true;
+        GameObject.Find("BottomQuad2").GetComponent<Renderer>().enabled = true;
+        GameObject.Find("TopLeftQuad2").GetComponent<Renderer>().enabled = true;
+        GameObject.Find("TopRightQuad2").GetComponent<Renderer>().enabled = true;
+        GameObject.Find("BottomLeftQuad2").GetComponent<Renderer>().enabled = true;
+        GameObject.Find("BottomRightQuad2").GetComponent<Renderer>().enabled = true;
     }
 
     // Update is called once per frame
@@ -22,14 +31,23 @@ public class FOVBehaviour : MonoBehaviour
     {
         if (OVRInput.GetDown(OVRInput.Button.Three))
         {
-            GameObject.Find("LeftQuad").GetComponent<Renderer>().enabled = !GameObject.Find("LeftQuad").GetComponent<Renderer>().enabled;
-            GameObject.Find("RightQuad").GetComponent<Renderer>().enabled = !GameObject.Find("RightQuad").GetComponent<Renderer>().enabled;
-            GameObject.Find("TopQuad").GetComponent<Renderer>().enabled = !GameObject.Find("TopQuad").GetComponent<Renderer>().enabled;
-            GameObject.Find("BottomQuad").GetComponent<Renderer>().enabled = !GameObject.Find("BottomQuad").GetComponent<Renderer>().enabled;
-            GameObject.Find("TopLeftQuad").GetComponent<Renderer>().enabled = !GameObject.Find("TopLeftQuad").GetComponent<Renderer>().enabled;
-            GameObject.Find("TopRightQuad").GetComponent<Renderer>().enabled = !GameObject.Find("TopRightQuad").GetComponent<Renderer>().enabled;
-            GameObject.Find("BottomLeftQuad").GetComponent<Renderer>().enabled = !GameObject.Find("BottomLeftQuad").GetComponent<Renderer>().enabled;
-            GameObject.Find("BottomRightQuad").GetComponent<Renderer>().enabled = !GameObject.Find("BottomRightQuad").GetComponent<Renderer>().enabled;
+            GameObject.Find("LeftQuad1").GetComponent<Renderer>().enabled = !GameObject.Find("LeftQuad1").GetComponent<Renderer>().enabled;
+            GameObject.Find("RightQuad1").GetComponent<Renderer>().enabled = !GameObject.Find("RightQuad1").GetComponent<Renderer>().enabled;
+            GameObject.Find("TopQuad1").GetComponent<Renderer>().enabled = !GameObject.Find("TopQuad1").GetComponent<Renderer>().enabled;
+            GameObject.Find("BottomQuad1").GetComponent<Renderer>().enabled = !GameObject.Find("BottomQuad1").GetComponent<Renderer>().enabled;
+            GameObject.Find("TopLeftQuad1").GetComponent<Renderer>().enabled = !GameObject.Find("TopLeftQuad1").GetComponent<Renderer>().enabled;
+            GameObject.Find("TopRightQuad1").GetComponent<Renderer>().enabled = !GameObject.Find("TopRightQuad1").GetComponent<Renderer>().enabled;
+            GameObject.Find("BottomLeftQuad1").GetComponent<Renderer>().enabled = !GameObject.Find("BottomLeftQuad1").GetComponent<Renderer>().enabled;
+            GameObject.Find("BottomRightQuad1").GetComponent<Renderer>().enabled = !GameObject.Find("BottomRightQuad1").GetComponent<Renderer>().enabled;
+
+            GameObject.Find("LeftQuad2").GetComponent<Renderer>().enabled = !GameObject.Find("LeftQuad2").GetComponent<Renderer>().enabled;
+            GameObject.Find("RightQuad2").GetComponent<Renderer>().enabled = !GameObject.Find("RightQuad2").GetComponent<Renderer>().enabled;
+            GameObject.Find("TopQuad2").GetComponent<Renderer>().enabled = !GameObject.Find("TopQuad2").GetComponent<Renderer>().enabled;
+            GameObject.Find("BottomQuad2").GetComponent<Renderer>().enabled = !GameObject.Find("BottomQuad2").GetComponent<Renderer>().enabled;
+            GameObject.Find("TopLeftQuad2").GetComponent<Renderer>().enabled = !GameObject.Find("TopLeftQuad2").GetComponent<Renderer>().enabled;
+            GameObject.Find("TopRightQuad2").GetComponent<Renderer>().enabled = !GameObject.Find("TopRightQuad2").GetComponent<Renderer>().enabled;
+            GameObject.Find("BottomLeftQuad2").GetComponent<Renderer>().enabled = !GameObject.Find("BottomLeftQuad2").GetComponent<Renderer>().enabled;
+            GameObject.Find("BottomRightQuad2").GetComponent<Renderer>().enabled = !GameObject.Find("BottomRightQuad2").GetComponent<Renderer>().enabled;
         }
     }
 }
