8d19b7edc6bb335cf5644bee1cbccbbd49f1b75e
Ramin Atrian <ratrian@ucsd.edu>
2021-05-18 00:54:10 -0700

    progress for part #7, done with part #8

--- Assets/Scripts/LagBehaviour.cs
+++ Assets/Scripts/LagBehaviour.cs
@@ -5,13 +5,18 @@ using UnityEngine;
 
 public class LagBehaviour : MonoBehaviour
 {
-    int numFramesTrackingLag, numFramesRenderingDelay;
+    int numFramesTrackingLag, numFramesRenderingDelay, i;
+    List<Vector3> leftEyePos = new List<Vector3>();
+    List<Vector3> rightEyePos = new List<Vector3>();
+    List<Quaternion> leftEyeRot = new List<Quaternion>();
+    List<Quaternion> rightEyeRot = new List<Quaternion>();
 
     // Start is called before the first frame update
     void Start()
     {
         numFramesTrackingLag = 0;
         numFramesRenderingDelay = 0;
+        i = -1;
     }
 
     // Update is called once per frame
@@ -23,15 +28,48 @@ public class LagBehaviour : MonoBehaviour
             if (numFramesTrackingLag > 0)
             {
                 numFramesTrackingLag--;
+                i = -1;
+                leftEyePos.Clear();
+                rightEyePos.Clear();
+                leftEyeRot.Clear();
+                rightEyeRot.Clear();
             }
-            //
         }
         else if (OVRInput.GetDown(OVRInput.Button.SecondaryIndexTrigger))
         {
             numFramesTrackingLag++;
-            //
+            i = -1;
+            leftEyePos.Clear();
+            rightEyePos.Clear();
+            leftEyeRot.Clear();
+            rightEyeRot.Clear();
         }
         GameObject.Find("TrackingLag").GetComponent<UnityEngine.UI.Text>().text = "Tracking Lag:\n" + numFramesTrackingLag.ToString() + " frames";
+        if (i < numFramesTrackingLag - 1)
+        {
+            i++;
+            leftEyePos.Add(GameObject.Find("lparent").transform.position);
+            rightEyePos.Add(GameObject.Find("rparent").transform.position);
+            leftEyeRot.Add(GameObject.Find("lparent").transform.rotation);
+            rightEyeRot.Add(GameObject.Find("rparent").transform.rotation);
+        }
+        else if ((i != -1) && (i == numFramesTrackingLag - 1))
+        {
+            leftEyePos.Add(GameObject.Find("lparent").transform.position);
+            rightEyePos.Add(GameObject.Find("rparent").transform.position);
+            leftEyeRot.Add(GameObject.Find("lparent").transform.rotation);
+            rightEyeRot.Add(GameObject.Find("rparent").transform.rotation);
+
+            /*GameObject.Find("lparent").transform.position = leftEyePos[0];
+            GameObject.Find("rparent").transform.position = rightEyePos[0];
+            GameObject.Find("lparent").transform.rotation = leftEyeRot[0];
+            GameObject.Find("rparent").transform.rotation = rightEyeRot[0];*/
+
+            leftEyePos.RemoveAt(0);
+            rightEyePos.RemoveAt(0);
+            leftEyeRot.RemoveAt(0);
+            rightEyeRot.RemoveAt(0);
+        }
 
         // Rendering Lag
         if (OVRInput.GetDown(OVRInput.Button.PrimaryHandTrigger))
